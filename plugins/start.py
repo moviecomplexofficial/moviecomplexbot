023c126128e3ed117500f3c03186950d097e6e81
Rose <123132237+Rosestorycity@users.noreply.github.com>
2023-04-26 23:06:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,6 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
+                    InlineKeyboardButton('📢 MC Adzz', url='https://t.me/mc_adzz'),
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
